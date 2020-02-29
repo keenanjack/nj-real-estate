8677e5dcf53ba8d30a5acd1f11f937a8fbf16941
Jack Keenan <keenan.jack3@gmail.com>
2020-02-29 12:15:46 -0500

    first view for looking up locations

--- njrealestate/views.py
+++ njrealestate/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from .models import Municipalities
+
+def property_locations(request):
+    unique_counties = Municipalities.objects.values()
+    unique_county_list = list(unique_counties)
+    return JsonResponse(unique_county_list, safe=False)
 
 # Create your views here.
